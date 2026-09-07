8bbd3bd6741dea2a34639ee3df63ac94b8c5e722
Yevhenii Riabokon <yevhenii.riabokon@dci-student.org>
2026-09-07 23:27:55 +0200

    design: enlarge gewerk card titles by 30%

--- web/src/app/(marketing)/page.tsx
+++ web/src/app/(marketing)/page.tsx
@@ -95,7 +95,7 @@ export default async function Home() {
                       </div>
                     )}
                     <span className="px-5 py-4">
-                      <span className="font-semibold leading-snug text-[#008574]">
+                      <span className="font-semibold leading-snug text-[1.3rem] text-[#008574]">
                         {trade}
                       </span>
                     </span>
